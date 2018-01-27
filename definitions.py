a23d6dbb78dc145e190c782140f2b609a8c5f6d4
Unknown <chri.geo@hotmail.fr>
2018-01-27 14:09:14 +0100

    Add util_random test

--- definitions.py
+++ definitions.py
@@ -1,3 +1,4 @@
 from os.path import dirname, abspath
 
 ROOT_DIR = dirname(abspath(__file__))
+TESTS_DIR = ROOT_DIR + "/tests/"
---
+++ tests/tests_utils/test_utils_random.py
@@ -0,0 +1,7 @@
+from utils.utils_random import get_random_string_from_list
+
+
+class TestUtilsRandom(object):
+    def test_get_random_string_from_list(self):
+        str_list = ["1", "2", "3"]
+        assert get_random_string_from_list(str_list) in str_list
